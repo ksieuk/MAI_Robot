1e56e306aacbe134970a74e9af222bc919455d04
Prokofiev Sergey <100406759+Dregtins@users.noreply.github.com>
2023-03-20 23:17:30 +0300

    Add files via upload

---
+++ Robot.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Position.h"
+
+class Robot {
+public:
+	void moveForward();
+	void setAngle();
+	void moveBack();
+	void stop();
+};
+
---
+++ User.cpp
@@ -0,0 +1,6 @@
+#include "User.h"
+#include <iostream>
+
+void User::makeOrder() {
+	std::cout << "Make Order";
+}
